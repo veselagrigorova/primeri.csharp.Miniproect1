8aebb90b07fc549dfce62e19acd230f6e99fe4b3
весела <adresza@abv.bg>
2015-12-05 15:51:57 +0200

    Модул06.Видео03.dllфайлове

---
+++ MP01/CALLCULATOR/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CALLCULATOR
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
---
+++ MP01/Userinnput/Userinnput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Userinnput
+{
+	public class UInnput
+	{
+		public UInnput ()
+		{
+		}
+
+		public void justTesting ()
+		{
+			Console.WriteLine ("Стартиране на Userinnput.UInnput.justTesting ()");
+		}
+	}
+}
+
