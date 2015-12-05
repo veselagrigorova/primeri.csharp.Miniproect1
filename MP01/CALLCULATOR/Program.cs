e714fc5c968b002ec9491163e262ca639f567369
весела <adresza@abv.bg>
2015-12-05 17:25:27 +0200

    Модул06. Основни принципи на архитектура на програма

--- MP01/CALLCULATOR/Program.cs
+++ MP01/CALLCULATOR/Program.cs
@@ -4,9 +4,17 @@ namespace CALLCULATOR
 {
 	class MainClass
 	{
+		//Дефиниране на библиотеки
+
+		public Userinnput.UInnput uinput = new	Userinnput.UInnput ();
+
+
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Hello World!");
+			MainClass _program = new MainClass ();
+
+			_program.uinput.justTesting ();
 		}
 	}
 }
