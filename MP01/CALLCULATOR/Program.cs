f6af9cce8da6e0be78bc533e73165ccde1c76966
весела <adresza@abv.bg>
2015-12-06 11:33:19 +0200

    Модул06.Управление с команди

--- MP01/CALLCULATOR/Program.cs
+++ MP01/CALLCULATOR/Program.cs
@@ -11,10 +11,13 @@ namespace CALLCULATOR
 
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
 			MainClass _program = new MainClass ();
 
-			_program.uinput.justTesting ();
+			_program.uinput. getUserCommands ();
+
+
+
+
 		}
 	}
 }
--- MP01/Userinnput/Userinnput.cs
+++ MP01/Userinnput/Userinnput.cs
@@ -7,7 +7,24 @@ namespace Userinnput
 		public UInnput ()
 		{
 		}
+		public void getUserCommands ()
+		{
+			string _command = "";
+			do{
+				//Взимане на команда
 
+				Console.Write ("$");
+				_command = Console.ReadLine();
+
+				//Проверка на налични команди
+				if ( _command.ToLower().Contains ("команда1" )) 	Console.WriteLine ("Стартирана е Команда 1\n");  
+				if ( _command.ToLower().Contains ("команда2" )) 	Console.WriteLine ("Стартирана е Команда 2\n");
+				if ( _command.ToLower().Contains ("команда3" ))		Console.WriteLine ("Стартирана е Команда 3 \n");
+
+
+			}while(_command.ToLower() != "изход");
+		}
+				
 		public void justTesting ()
 		{
 			Console.WriteLine ("Стартиране на Userinnput.UInnput.justTesting ()");
